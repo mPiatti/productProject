e00652b322b8212a3f6388c3fafdfd11d1e94684
Maurizio Piatti <mpiatti.develop@gmail.com>
2017-11-19 15:20:46 +0100

    added onPrePersit function for set the created_at value on insert

--- src/ProductBundle/Entity/Product.php
+++ src/ProductBundle/Entity/Product.php
@@ -12,44 +12,54 @@ use Doctrine\Common\Collections\ArrayCollection;
  */
 class Product
 {
-  /**
-   * @ORM\Column(type="integer", name="product_id")
-   * @ORM\Id
-   * @ORM\GeneratedValue(strategy="AUTO")
-   */
-  private $productId;
+    /**
+     * @ORM\Column(type="integer", name="product_id")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $productId;
 
-  /**
-   * @ORM\Column(type="string")
-   */
-  private $name;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
 
-  /**
-   * @ORM\Column(type="text", nullable=true)
-   */
-  private $description;
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
 
-  /**
-   * @ORM\Column(type="string", name="main_image", nullable=true)
-   */
-  private $mainImage;
+    /**
+     * @ORM\Column(type="string", name="main_image", nullable=true)
+     */
+    private $mainImage;
 
-  /**
-   * @ORM\Column(type="datetime", name="created_at")
-   */
-  private $createdAt;
+    /**
+     * @ORM\Column(type="datetime", name="created_at")
+     */
+    private $createdAt;
 
-  /**
-   * Many Products have many tags
-   * @ORM\ManyToMany(targetEntity="Tag", inversedBy="products")
-   * @ORM\JoinTable(name="products_tags",
-   *      joinColumns={@ORM\JoinColumn(name="product_id", referencedColumnName="product_id")},
-   *      inverseJoinColumns={@ORM\JoinColumn(name="tag_id", referencedColumnName="tag_id")}
-   *      )
-   */
-  private $tags;
+    /**
+     * Many products can have many tags
+     * @ORM\ManyToMany(targetEntity="Tag", inversedBy="products")
+     * @ORM\JoinTable(name="products_tags",
+     *      joinColumns={@ORM\JoinColumn(name="product_id", referencedColumnName="product_id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="tag_id", referencedColumnName="tag_id")}
+     *      )
+     */
+    private $tags;
 
-  public function __construct() {
-    $this->tags = new ArrayCollection();
-  }
+    public function __construct() {
+        $this->tags = new ArrayCollection();
+    }
+
+    /**
+     * Set the created at value: trigger on insert
+     *
+     * @ORM\PrePersist
+     */
+    public function onPrePersist()
+    {
+        $this->createdAt = new \DateTime('now');
+    }
 }
