fa8d6377608fd3643a770071acb6bda5fe2d88c9
Maurizio Piatti <mpiatti.develop@gmail.com>
2017-11-20 00:02:27 +0100

    write getter and setter Tag entity

--- src/ProductBundle/Entity/Tag.php
+++ src/ProductBundle/Entity/Tag.php
@@ -11,25 +11,47 @@ use Doctrine\Common\Collections\ArrayCollection;
  */
 class Tag
 {
-  /**
-   * @ORM\Column(type="integer", name="tag_id")
-   * @ORM\Id
-   * @ORM\GeneratedValue(strategy="AUTO")
-   */
-  private $tagId;
+    /**
+     * @ORM\Column(type="integer", name="tag_id")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $tagId;
 
-  /**
-   * @ORM\Column(type="string")
-   */
-  private $name;
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
 
-  /**
-   * Many tags can have many products
-   * @ORM\ManyToMany(targetEntity="Product", mappedBy="tags")
-   */
-  private $products;
+    /**
+     * Many tags can have many products.
+     *
+     * @ORM\ManyToMany(targetEntity="Product", mappedBy="tags")
+     */
+    private $products;
 
-  public function __construct() {
-    $this->products = new ArrayCollection();
-  }
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    /**
+     * Tag name setter
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Tag name getter
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 }
