9ca8b7a0f9a49c4298036ba78dc44fa3d7412233
Maurizio Piatti <mpiatti.develop@gmail.com>
2017-11-20 22:20:33 +0100

    update product entity for image upload

--- src/ProductBundle/Entity/Product.php
+++ src/ProductBundle/Entity/Product.php
@@ -4,11 +4,16 @@ namespace ProductBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Entity\File as EmbeddedFile;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="product")
  * @ORM\HasLifecycleCallbacks
+ * @Vich\Uploadable
  */
 class Product
 {
@@ -16,28 +21,52 @@ class Product
      * @ORM\Column(type="integer", name="product_id")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
+     *
+     * @var int
      */
     private $productId;
 
     /**
      * @ORM\Column(type="string")
+     *
+     * @var string
      */
     private $name;
 
     /**
      * @ORM\Column(type="text", nullable=true)
+     *
+     * @var string
      */
     private $description;
 
     /**
-     * @ORM\Column(type="string", name="main_image", nullable=true)
+     * @ORM\Column(type="string", length=255)
+     *
+     * @var string
      */
-    private $mainImage;
+    private $image;
+
+    /**
+     * @Vich\UploadableField(mapping="product_image", fileNameProperty="image")
+     *
+     * @var File
+     */
+    private $imageFile;
 
     /**
      * @ORM\Column(type="datetime", name="created_at")
+     *
+     * @var \DateTime
      */
     private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", name="updated_at")
+     *
+     * @var \DateTime
+     */
+    private $updatedAt;
 
     /**
      * Many products can have many tags.
@@ -47,6 +76,8 @@ class Product
      *      joinColumns={@ORM\JoinColumn(name="product_id", referencedColumnName="product_id")},
      *      inverseJoinColumns={@ORM\JoinColumn(name="tag_id", referencedColumnName="tag_id")}
      *      )
+     *
+     * @var ArrayCollection
      */
     private $tags;
 
@@ -56,8 +87,6 @@ class Product
     }
 
     /**
-     * Name setter
-     *
      * @param string $name
      */
     public function setName($name)
@@ -66,8 +95,6 @@ class Product
     }
 
     /**
-     * Name getter
-     *
      * @return string
      */
     public function getName()
@@ -76,8 +103,6 @@ class Product
     }
 
     /**
-     * Description setter
-     *
      * @param string $description
      */
     public function setDescription($description)
@@ -86,8 +111,6 @@ class Product
     }
 
     /**
-     * Description getter
-     *
      * @return string
      */
     public function getDescription()
@@ -96,32 +119,54 @@ class Product
     }
 
     /**
-     * Main image setter
-     *
-     * @param string $mainImage
+     * @param File $image
      */
-    public function setMainImage($mainImage)
+    public function setImageFile(File $image = null)
     {
-        $this->mainImage = $mainImage;
+        $this->imageFile = $image;
+
+        if ($image) {
+            $this->updatedAt = new \DateTime('now');
+        }
     }
 
     /**
-     * Main image getter
-     *
+     * @return File
+     */
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    /**
+     * @param string $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
      * @return string
      */
-    public function getMainImage()
+    public function getImage()
     {
-        return $this->mainImage;
+        return $this->image;
     }
 
     /**
-     * Set the created at value: trigger on insert.
-     *
      * @ORM\PrePersist
      */
     public function onPrePersist()
     {
-        $this->createdAt = new \DateTime('now');
+        $this->updatedAt = $this->createdAt = new \DateTime('now');
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function onPreUpdate()
+    {
+        $this->updatedAt = new \DateTime('now');
     }
 }
