d65bb62802c6c408d019d95a250aa8c1465844e5
Maurizio Piatti <mauriziopiatti@artera.it>
2017-11-29 17:21:26 +0100

    installation formvalidation.io

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -2,6 +2,8 @@ var $ = require('jquery');
 
 import 'bootstrap';
 import 'bootstrap-tagsinput';
+import 'formvalidation';
+import 'formvalidation/dist/js/framework/bootstrap';
 
 // bootstrap-tagsinput
 $('input[data-role="tagsinput"]').tagsinput({
@@ -9,3 +11,22 @@ $('input[data-role="tagsinput"]').tagsinput({
 });
 
 // formvalidation.io
+$(document).ready(function () {
+    $('#productForm')
+        .find('[name="product[name]"]')
+            .change(function (e) {
+                $('#productForm').formValidation('revalidateField', 'name');
+            })
+            .end()
+        .formValidation({
+            fields: {
+                name: {
+                    validators: {
+                        notEmpty: {
+                            message: 'required field'
+                        }
+                    }
+                }
+            }
+        });
+});
